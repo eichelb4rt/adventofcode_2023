db00971e7edd4183f6536d242a2d3f3a2fa19b9b
eichelb4rt <maurice.wenig@gmail.com>
2023-12-05 14:03:23 +0100

    automatic preparation

---
+++ day_01/part_1.py
@@ -0,0 +1,14 @@
+def solution(input_file: str):
+    with open(input_file, 'r') as f:
+        lines = f.read()
+    return None
+
+
+def main():
+    assert solution("test_input.txt") == EXPECTED_SOLUTION
+    answer = solution("input.txt")
+    print(f"<flavor text> {answer}.")
+
+
+if __name__ == "__main__":
+    main()
