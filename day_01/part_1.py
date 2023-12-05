2c8f3cf3e8ce01eeec7cb1bed58d7e7d6dce1ea1
eichelb4rt <maurice.wenig@gmail.com>
2023-12-05 14:32:04 +0100

    day 01 done, removed day 02

--- day_01/part_1.py
+++ day_01/part_1.py
@@ -1,13 +1,21 @@
+DIGITS = list(map(str, range(10)))
+
+
+def digits_in_line(line: str) -> list[int]:
+    return [int(c) for c in line if c in DIGITS]
+
+
 def solution(input_file: str):
     with open(input_file, 'r') as f:
-        lines = f.read()
-    return None
+        lines = f.read().splitlines()
+    digits_per_line = [digits_in_line(line) for line in lines]
+    return sum([digits[0] * 10 + digits[-1] for digits in digits_per_line])
 
 
 def main():
-    assert solution("test_input.txt") == EXPECTED_SOLUTION
+    assert solution("test_input_1.txt") == 142
     answer = solution("input.txt")
-    print(f"<flavor text> {answer}.")
+    print(f"The calibration number is: {answer}")
 
 
 if __name__ == "__main__":
--- day_01/part_2.py
+++ day_01/part_2.py
@@ -1,13 +1,40 @@
+DIGITS = list(map(str, range(10)))
+WORD_TO_DIGIT = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def replace_words(line: str) -> str:
+    for word, digit in WORD_TO_DIGIT.items():
+        # append digit after word, so that this insertion doesn't destroy spelled out digits before this one
+        line = line.replace(word, word + str(digit))
+    return line
+
+
+def digits_in_line(line: str) -> list[int]:
+    return [int(c) for c in line if c in DIGITS]
+
+
 def solution(input_file: str):
     with open(input_file, 'r') as f:
-        lines = f.read()
-    return None
+        lines = f.read().splitlines()
+    lines = [replace_words(line) for line in lines]
+    digits_per_line = [digits_in_line(line) for line in lines]
+    return sum([digits[0] * 10 + digits[-1] for digits in digits_per_line])
 
 
 def main():
-    assert solution("test_input.txt") == EXPECTED_SOLUTION
+    assert solution("test_input_2.txt") == 281
     answer = solution("input.txt")
-    print(f"<flavor text> {answer}.")
+    print(f"The calibration number is: {answer}")
 
 
 if __name__ == "__main__":
